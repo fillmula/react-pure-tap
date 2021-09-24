07ce2fe3f0f2318b4834288b3e24b639e948557c
Victor Teo <victor.teo@fillmula.com>
2021-09-24 13:34:34 +0800

    add on class name

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,30 @@
-import React, {
-    useState, useEffect, createElement, FC, CSSProperties, ComponentProps
+import {
+    useState, useEffect, createElement, FC, CSSProperties, ComponentProps,
+    useRef
 } from 'react'
 
 interface PureTapProps {
     component: string
     className?: string
+    onClassName?: string
     style?: CSSProperties
     action?(): void
     direction?: 'horizontal' | 'vertical'
 }
 
 const PureTap: FC<PureTapProps> = ({
-    children, component = 'span', className = '__pure-tap', style, action, direction = 'vertical'
+    children,
+    component = 'span',
+    className = '__pure-tap',
+    onClassName = '__pure-tap-on',
+    style,
+    action,
+    direction = 'vertical'
 }) => {
     const [isOn, setIsOn] = useState(false)
     const [mouseDown, setMouseDown] = useState(false)
     const [shouldTriggerAction, setShouldTriggerAction] = useState(false)
-    const [point, setPoint] = useState<number[] | null>(null)
+    const pointRef = useRef<number[] | null>(null)
     useEffect(() => {
         let windowMouseUp: (this: Window, ev: MouseEvent) => any
         // If this is a mouse device instead of a touching device
@@ -35,60 +43,63 @@ const PureTap: FC<PureTapProps> = ({
     const handlers: () => ComponentProps<"span"> = () => {
         if (window.ontouchstart === undefined) {
             return {
-              onMouseDown: event => {
-                  setMouseDown(true)
-                  setIsOn(true)
-              },
-              onMouseUp: event => {
-                  setMouseDown(false)
-                  setIsOn(false)
-                  action && action()
-              },
-              onMouseLeave: event => {
-                  setMouseDown(false)
-                  setIsOn(false)
-              }
+                onMouseDown: event => {
+                    setMouseDown(true)
+                    setIsOn(true)
+                },
+                onMouseUp: event => {
+                    setMouseDown(false)
+                    setIsOn(false)
+                    action && action()
+                },
+                onMouseLeave: event => {
+                    setMouseDown(false)
+                    setIsOn(false)
+                }
             }
-          } else {
+        } else {
             return {
-              onTouchStart: event => {
-                  setShouldTriggerAction(true)
-                  setIsOn(true)
-                  setPoint([event.touches[0].clientX, event.touches[0].clientY])
-              },
-              onTouchMove: event => {
-                  if (!shouldTriggerAction) return;
-                  if (direction === 'vertical') {
-                    if (event.touches[0].clientY !== point![1]) {
-                        setShouldTriggerAction(false);
-                        setIsOn(false)
-                        setPoint(null)
-                      }
-                  } else if (direction === 'horizontal') {
-                    if (event.touches[0].clientX !== point![0]) {
-                        setShouldTriggerAction(false)
-                        setIsOn(false)
-                        setPoint(null)
+                onTouchStart: event => {
+                    setShouldTriggerAction(true)
+                    setIsOn(true)
+                    pointRef.current = [
+                        event.touches[0].clientX,
+                        event.touches[0].clientY
+                    ]
+                },
+                onTouchMove: event => {
+                    if (!shouldTriggerAction) return;
+                    if (direction === 'vertical') {
+                        if (event.touches[0].clientY !== pointRef.current![1]) {
+                            setShouldTriggerAction(false);
+                            setIsOn(false)
+                            pointRef.current = null
+                        }
+                    } else if (direction === 'horizontal') {
+                        if (event.touches[0].clientX !== pointRef.current![0]) {
+                            setShouldTriggerAction(false)
+                            setIsOn(false)
+                            pointRef.current = null
+                        }
                     }
-                  }
-              },
-              onTouchEnd: event => {
-                  if (shouldTriggerAction) {
-                      action && action()
-                  }
-                  setShouldTriggerAction(false)
-                  setIsOn(false)
-                  setPoint(null)
-              },
-              onTouchCancel: event => {
-                  setShouldTriggerAction(false)
-                  setIsOn(false)
-                  setPoint(null)
-              }
+                },
+                onTouchEnd: event => {
+                    if (shouldTriggerAction) {
+                        action && action()
+                    }
+                    setShouldTriggerAction(false)
+                    setIsOn(false)
+                    pointRef.current = null
+                },
+                onTouchCancel: event => {
+                    setShouldTriggerAction(false)
+                    setIsOn(false)
+                    pointRef.current = null
+                }
             }
-          }
+        }
     }
-    const props = { style, className: `${className}${isOn ? ' __pure-tap-on' : ''}`, ...handlers() }
+    const props = { style, className: `${className}${isOn ? ` ${onClassName}` : ''}`, ...handlers() }
     return createElement(component, props, children)
 }
 
